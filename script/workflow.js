3c160a7ad430ce3aac8d608d63668c8b3c5c446e
BetterWayComputing <dbrussee@gmail.com>
2020-10-12 11:26:26 -0400

    Update workflow.js

--- script/workflow.js
+++ script/workflow.js
@@ -35,9 +35,10 @@ WF.handleMouseUp = function(event) {
             WF.pickedItem.x = newx;
             WF.pickedItem.y = newy;
             WFUI.dragstart.item = null;
+            WF.drawCanvas();
         }
         WFUI.dragstart = null;
-        WF.drawCanvas();
+        app.saveLocal();
     }
 }
 WF.handleMouseDown = function(event) {
